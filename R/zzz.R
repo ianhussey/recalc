5d0e1dfacdf07df3a6f635735a25efd2eb1b6339
Ian Hussey <ianhusseysipod@gmail.com>
2024-03-03 11:53:33 +0100

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#' recalc
+#'
+#' Recalculate statistical results from reported summary and test statistics.
+#'
+#' @section Important:
+#' Package is in early stage development.
+#'
+#' @docType package
+#' @name ERROR
+NULL
